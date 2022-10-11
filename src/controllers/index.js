076a097e02511847025635b5f73855ffb3c7a08d
Marcelo Mariano <marcelomariano11@gmail.com>
2022-10-11 09:49:58 -0300

    index.js bug fixes - add let getAllData to fix

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -9,7 +9,27 @@ let indexControlexibe = async (req, res) => {
     stocksList.push(req.body.stock)
 
 
-    let getData = await getMktData(stocksList)
+    let getaLLData = await getMktData(stocksList)
+    let getData = [{
+        symbol: getaLLData[0].symbol,
+        name: getaLLData[0].name,
+        open: getaLLData[0].open,
+        high: getaLLData[0].high,
+        low: getaLLData[0].low,
+        close: getaLLData[0].close,
+        volume: getaLLData[0].volume,
+        financial_volume: getaLLData[0].financial_volume,
+        bid: getaLLData[0].bid,
+        ask: getaLLData[0].ask,
+        variation: getaLLData[0].variation,
+        iv_1y_rank: getaLLData[0].iv_1y_rank,
+        iv_current: getaLLData[0].iv_current,
+        stdv_1y: (getaLLData[0].stdv_1y),
+        stdv_5d: (getaLLData[0].stdv_5d),
+        date: getaLLData[0].date
+    }]
+
+    console.log(getData)
 
     res.render('exibe.ejs', { getData: getData })
 
